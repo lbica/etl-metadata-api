be66442a46e734afb93e9625f00c33b3556438d8
lbica <laurentiubica@yahoo.com>
2021-02-01 17:40:36 +0100

    Solve Heroku deployments errors

--- default_config.py
+++ default_config.py
@@ -5,7 +5,7 @@ This is for development. The config.py will override the values for production
 import os
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = True
 UPLOADED_IMAGES_DEST = os.path.join("static", "images")
